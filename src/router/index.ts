318292fe34754ea7f5b22b98cdaa2415bfd96cec
xh <2715782947@qq.com>
2021-03-13 16:10:28 +0800

    体育馆场地预约管理系统

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,14 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'LogIn',
+    component: () => import(/* webpackChunkName: "LogIn" */ '../views/LogIn.vue')
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/LogIn.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>登录页面</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class LogIn extends Vue {}
+</script>
